8d304e355c590a768822fdf8fa02f852a62c1cdc
Aries1711 <levfreey17@gmail.com>
2024-05-15 00:52:55 +0700

    fix : renew declaration runner file

--- JAVASCRIPT/MAIN/index.js
+++ JAVASCRIPT/MAIN/index.js
@@ -1,43 +1,19 @@
-var data = [
-  [18, 20],
-  [45, 2],
-  [61, 12],
-  [37, 6],
-  [21, 21],
-  [78, 9],
-];
+// DEBUG DECLARE VARIABLE HERE
+// var data = [
+//   [18, 20],
+//   [45, 2],
+//   [61, 12],
+//   [37, 6],
+//   [21, 21],
+//   [78, 9],
+// ];
 
-openOrSenior(data);
+// DEBUG FUNCTION HERE
+// openOrSenior(data);
 
-function openOrSenior(data) {
-  //   console.log(
-  //     [].data.map(function (element) {
-  //       if (element[0] >= 55 && element[1] >= 7) {
-  //         return "Senior";
-  //       } else {
-  //         return "Open";
-  //       }
-  //     })
-  //   );
-  //   return [].data.map(function (element) {
-  //     if (element[0] >= 55 && element[1] >= 7) {
-  //       return "Senior";
-  //     } else {
-  //       return "Open";
-  //     }
-  //   });
-
-  return [...data].map((element) =>
-    element[0] >= 55 && element[1] >= 7 ? "Senior" : "Open"
-  );
-
-  //   data.map(function (element) {
-  //     if (element[0] >= 55 && element[1] >= 7) {
-  //       console.log("Senior");
-  //       return "Senior";
-  //     } else {
-  //       console.log("Open");
-  //       return "Open";
-  //     }
-  //   });
-}
+// DEBUG FUNCTION SOLUTION HERE
+// function openOrSenior(data) {
+//   return [...data].map((element) =>
+//     element[0] >= 55 && element[1] >= 7 ? "Senior" : "Open"
+//   );
+// }
